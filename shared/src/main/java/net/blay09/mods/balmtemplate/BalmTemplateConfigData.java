fe18f7ca6486815329b81570acdd524dd9ab1b7d
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2022-12-15 21:58:19 +0100

    build: General cleanup

--- shared/src/main/java/net/blay09/mods/balmtemplate/BalmTemplateConfigData.java
+++ shared/src/main/java/net/blay09/mods/balmtemplate/BalmTemplateConfigData.java
@@ -2,7 +2,6 @@ package net.blay09.mods.balmtemplate;
 
 import me.shedaniel.autoconfig.annotation.Config;
 import net.blay09.mods.balm.api.config.BalmConfigData;
-import net.blay09.mods.balm.api.config.Comment;
 
 @Config(name = BalmTemplate.MOD_ID)
 public class BalmTemplateConfigData implements BalmConfigData {
