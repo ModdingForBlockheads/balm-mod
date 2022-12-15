06fe368a652100ea4449886cc9314d72cfa685a0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2022-12-15 18:44:54 +0100

    feat: Add data gen template, update gradle files

---
+++ fabric/src/main/java/net/blay09/mods/balmtemplate/datagen/BalmTemplateDataGenerator.java
@@ -0,0 +1,11 @@
+package net.blay09.mods.balmtemplate.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BalmTemplateDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
