a6d9e401fa4c9edf4df311c9f2e24c448e51ca7f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-17 23:54:53 +0100

    fix: Fix wrong ClientModInitializer

--- fabric/src/main/java/yourname/mods/yourmod/fabric/client/FabricYourModClient.java
+++ fabric/src/main/java/yourname/mods/yourmod/fabric/client/FabricYourModClient.java
@@ -2,13 +2,13 @@ package yourname.mods.yourmod.fabric.client;
 
 import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import yourname.mods.yourmod.YourMod;
 import yourname.mods.yourmod.client.YourModClient;
 
-public class FabricYourModClient implements ModInitializer {
+public class FabricYourModClient implements ClientModInitializer {
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         BalmClient.initialize(YourMod.MOD_ID, EmptyLoadContext.INSTANCE, YourModClient::initialize);
     }
 }
